969f1aa3b6470dbe967d56ae7feca254692b0069
Kevin Liao <liaok0082@gmail.com>
2021-09-04 22:03:34 +0800

    Class 10 - conditionals

--- scripts.js
+++ scripts.js
@@ -1,64 +1,19 @@
-// Datatypes and variables
+// Distance in miles
+let distance = 5;
 
-// Variable: A piece of data that is stored in the computer
-let age; // I created a variable called "age". Define a variable
+// Constants: Like a variable BUT it does not change
+const NUMBER = 1.6;
+// consts CAN NOT be modified
 
-// Assign a value to my variable
-age = 90; // Assign a value to a variable
+// 1.6 x distance
+let distanceInKm = distance * NUMBER;
 
-// Comments are not "Visible" form Js
+console.log(distance + ' miles is equal to ' + distanceInKm + ' km');
 
-// Re assign a value (change the value of the variable)
-age = 15;
-
-/**
- * Naming the variables
- *
- * We can name a variable as we want, BUT
- * Variable's names can contain letters, numbers, _ or $
- * The names CAN NOT start with a number
- * The variables names MUST be compounded of one single word
- * We can not have two varriables with the same name
- * We CAN NOT use spetial words (reserved words)
- *
- * Cammel case
- * let name;
- * let nameOfTheUser;
- * let dateOfBirth;
- * We separate the words using an uppercase instead of a space
- *
- * Snake case (?)
- * let name_of_the_user;
- * let date_of_brith;
- *
- * let, var, const, number, function, if, else, switch, class
- */
-
-// Inicialize a variable
-let name;
-// Assign a value
-name = 'Kevin';
-
-// Initialize and assign a value to a variable
-let birthday = '19 feb';
-
-// Datatypes
-
-// numbers
-let year = 2021; //  integer
-let temperature = -39.0; // negative
-
-let time = 9.9; // float
-let weight = 70.5;
-
-// strings (word/phrase)
-let myName = 'Kevin';
-let myBirthday = '19 february of 2002';
-
-let height = '174';
-let height2 = 174;
-
-// boolean (true / false)
-// Booleans have only two values
-let amITall = false;
-let didIPassTheExam = true;
+// Change the value of the variable
+// Reassingning the value of variables
+distance = 10;
+distanceInKm = distance * NUMBER;
+// We can not re assign values of Constants
+// NUMBER = 1.7; //ERRORR
+console.log(distance + ' miles is equal to ' + distanceInKm + ' km');
--- scripts2.js
+++ scripts2.js
@@ -1,105 +1,34 @@
-let name = 'Kevin';
-let surname = 'Liao';
+// Conditionals
+let age = 20;
 
-console.log(name);
+// if statemet
+if (age > 18) {
+	// Actions that will be performed if the condition is TRUE
+	console.log('You are over 18');
+} else if (age > 14) {
+	console.log('You are over 14');
+} else {
+	// The else statement is excecuted if NONE OF the previous conditions were satisfied
+	console.log('You are too small');
+}
 
-let age = 15;
+// ------------
 
-console.log(age);
+// This two if statements are NOT joined
 
-age = 19;
+if (age > 18) {
+	console.log('You are over 18');
+}
 
-console.log(age);
+// ------------
+if (age > 14) {
+	console.log('You are over 14');
+}
 
-let randomNumber = 80 - 70;
-let randomNumber2 = -20;
+// 0 - 100
+let grade = 60;
 
-// Sum operation
-let total = randomNumber + randomNumber2;
-console.log(total);
-
-// Substraction
-let total2 = randomNumber2 - randomNumber2;
-console.log(total2);
-
-// Multiplication
-let total3 = total * randomNumber;
-console.log(total3); // 17 * 8
-
-// Division
-let total4 = total / randomNumber;
-console.log(total4);
-
-// Concatenate: Sum strings
-let total5 = name + surname;
-console.log(total5);
-
-let total6 = name + ' ' + surname;
-console.log(total6);
-
-age = 20;
-let total7 = 'Im ' + age + ' years old';
-console.log(total7); // Im 19 years old
-
-// Compare numbers
-let a = 90;
-let b = 900;
-
-// Is a greater than b????
-let result1 = a >= b;
-console.log(result1);
-
-let result2 = a < b;
-console.log(result2);
-
-// Is a equal to b???
-let result3 = a === b;
-console.log(result3);
-
-// Is a different to b???
-// Is a NOT EQUAL to b???
-let result5 = a !== b; // ! means NOT
-console.log(result5);
-
-// The = sign is used for assignation
-a = 80; // Assignation: used to give a value to a variabel
-a === 80; // Comparison: Check for a condition
-
-let c = 80; // number
-let d = '80'; // string
-
-let result4 = c === d;
-console.log(result4);
-
-let result6 = a === a;
-console.log(result6);
-
-// Incrementation
-let counter = 0;
-counter++;
-counter++;
-counter++;
-counter = counter + 1;
-
-counter += 5;
-// counter = counter + 5;
-
-// Decrementing
-counter--;
-counter = counter - 1;
-
-counter -= 7;
-// counter = counter - 7
-counter++;
-
-counter *= 5;
-// counter = counter * 5;
-
-counter /= 5;
-// counter = counter / 5;
-
-console.log(counter);
-
-let counter2 = 'ABC';
-counter2++;
-console.log(counter2);
+// > 90 "You got an A"
+// > 70 "You got a B"
+// > 60 "You got a C"
+// < 60 "You failded"
